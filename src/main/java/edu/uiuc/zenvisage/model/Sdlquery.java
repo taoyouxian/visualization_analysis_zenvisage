af2d8e1172d6a7f7186ffcd644b61ae6609b886b
siddta <tarique.nitk@gmail.com>
2017-06-15 12:58:55 -0500

    SDL Model

---
+++ src/main/java/edu/uiuc/zenvisage/model/Sdlquery.java
@@ -0,0 +1,50 @@
+package edu.uiuc.zenvisage.model;
+/* @tarique
+ * 
+ */
+public class Sdlquery {
+	public String x;
+	public String y;
+	public String z;
+	public String dataset;
+	public String sdlsegments;
+	public String sdltext;
+	public Sdlquery(){};
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+	public String getZ() {
+		return z;
+	}
+	public void setZ(String z) {
+		this.z = z;
+	}
+	public String getSdlsegments() {
+		return sdlsegments;
+	}
+	public void setSdlsegments(String sdlsegments) {
+		this.sdlsegments = sdlsegments;
+	}
+	public String getSdltext() {
+		return sdltext;
+	}
+	public void setSdltext(String sdltext) {
+		this.sdltext = sdltext;
+	}
+	public String getDataset() {
+		return dataset;
+	}
+	public void setDataset(String dataset) {
+		this.dataset = dataset;
+	}
+
+}
